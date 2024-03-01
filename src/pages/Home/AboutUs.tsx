ddf4185fece029c7d8ab56e0b1d2bc83809c1016
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-01 22:58:52 +0600

    donation button added

--- src/pages/Home/AboutUs.tsx
+++ src/pages/Home/AboutUs.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 import Container from '../../components/ui/Containert'
 import HowItWorks from './HowItWorks'
 import Sponsors from "./Sponsors";
-
+import img from "../../assets/about_img.jpg";
+import SectionHeader from "../../utils/SectionHeader";
+import Donation from '../../utils/Donation';
 const AboutUs = () => {
   return (
     <div className="py-10 aboutBg">
       <Container>
+        <div>
+          <div>
+            <img src={img} alt="" />
+          </div>
+          <div>
+            <SectionHeader text="WHAT WE DO?" />
+            <Donation/>
+          </div>
+        </div>
         <HowItWorks />
         <Sponsors />
       </Container>
---
+++ src/utils/Donation.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { GiSelfLove } from "react-icons/gi";
+import "./Donation.css"; // Import the CSS file where you define the styles
+
+
+const Donation = () => {
+  const [hover, setHover] = useState(false);
+  const handleHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <div className=" py-5">
+      <button
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}
+        className=" z-50 duration-500 bg-green-400 text-xl text-white font-semibold px-4 py-2 flex items-center justify-center gap-2"
+      >
+        Donation
+        <GiSelfLove />
+      </button>
+      <button
+        className={` ${
+          hover ? "bottom-0.5 right-1.5 " : "top-2 left-1"
+        } -z-50 relative -mt-[48px] duration-1000 transform  bg-transparent border-[1px] border-green-400 text-xl text-transparent font-semibold px-4 py-2 flex items-center justify-center gap-2`}
+      >
+        Donation
+        <GiSelfLove />
+      </button>
+    </div>
+  );
+};
+
+export default Donation;
