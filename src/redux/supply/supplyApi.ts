c658f5f7061b1716eac39ea8b3af0d12f72ad2ca
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-02 18:01:33 +0600

    suppplyApi added

---
+++ src/redux/supply/supplyApi.ts
@@ -0,0 +1,14 @@
+import Gallery from "../../pages/Gallery/Gallery";
+import { baseApi } from "../baseApi";
+
+const supplyApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    supply: builder.query({
+      query: () => ({
+        url: "/supplies",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+export const {useSupplyQuery}=supplyApi;
