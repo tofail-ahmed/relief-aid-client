1cb377b9810391b532e2c4a7a8e0c70c75d00c12
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-05 08:20:51 +0600

    delete confirmation added

--- src/pages/Supplies/Supplies.tsx
+++ src/pages/Supplies/Supplies.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { useDeleteSupplyMutation, useSupplyQuery } from "../../redux/supply/supplyApi";
+import {
+  useDeleteSupplyMutation,
+  useSupplyQuery,
+} from "../../redux/supply/supplyApi";
 import { FcViewDetails } from "react-icons/fc";
 import { FaEdit } from "react-icons/fa";
-import {  MdAutoDelete } from "react-icons/md";
+import { MdAutoDelete } from "react-icons/md";
 
 import { Link } from "react-router-dom";
 import Modal from "../../components/Modal";
 import SupplyForm from "../../components/SupplyForm";
-interface ISupply{
-  _id:string;
+interface ISupply {
+  _id: string;
   title: string;
   category: string;
   amount: number;
@@ -17,111 +20,139 @@ interface ISupply{
   description: string;
 }
 const Supplies = () => {
-  const [deleteSupply]=useDeleteSupplyMutation();
   const [modal, setModal] = useState(false);
   const { data, isLoading } = useSupplyQuery("");
+  const [deleteSupply, { data: deleteData }] = useDeleteSupplyMutation();
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
-  const totalAmount = data.data.reduce((accumulator: number, currentValue: { amount?: number }) => {
-    // Ensure `amount` exists and is a number
-    const amount =
-      typeof currentValue.amount === "number" ? currentValue.amount : 0;
 
-    // Add the amount to the accumulator
-    return accumulator + amount;
-}, 0);
+  
 
-  console.log(totalAmount);
-  console.log(data.data);
+  const totalAmount = data.data.reduce(
+    (accumulator: number, currentValue: { amount?: number }) => {
+      // Ensure `amount` exists and is a number
+      const amount =
+        typeof currentValue.amount === "number" ? currentValue.amount : 0;
+
+      // Add the amount to the accumulator
+      return accumulator + amount;
+    },
+    0
+  );
+  const handleDelete = (id) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this supply?"
+    );
+    console.log(confirmDelete)
+    if (confirmDelete) {
+      deleteSupply(id);
+      alert("Data deleted successfully");
+    }
+   
+    
+  };
+  
+
+  
+
+  
+  console.log(deleteData);
   return (
-    <div className="text-black">
-      <h1 className="text-black text-4xl text-center">
-        This is supplies component
-      </h1>
-      <div>
-        <div className="overflow-x-auto">
-          <table className="table bg-slate-300">
-            {/* head */}
-            <thead>
-              <tr>
-                <th className="text-center">Title</th>
-                <th className="text-center">Category</th>
-                <th className="text-center">Donation</th>
-                <th className="text-center">Actions</th>
-              </tr>
-            </thead>
-            <tbody className="p-2">
-              {data?.data.map((supply: ISupply) => (
+    <div>
+      <div className="text-black">
+        <h1 className="text-black text-4xl text-center">
+          This is supplies component
+        </h1>
+        <div>
+          <div className="overflow-x-auto">
+            <table className="table bg-slate-300">
+              {/* head */}
+              <thead>
                 <tr>
-                  <td className="border-2 border-orange-500">
-                    <div className="flex items-center gap-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                          <img
-                            src={supply.image}
-                            alt="Avatar Tailwind CSS Component"
-                          />
+                  <th className="text-center">Title</th>
+                  <th className="text-center">Category</th>
+                  <th className="text-center">Donation</th>
+                  <th className="text-center">Actions</th>
+                </tr>
+              </thead>
+              <tbody className="p-2">
+                {data?.data.map((supply: ISupply) => (
+                  <tr>
+                    <td className="border-2 border-orange-500">
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img
+                              src={supply.image}
+                              alt="Avatar Tailwind CSS Component"
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">{supply.title}</div>
                         </div>
                       </div>
-                      <div>
-                        <div className="font-bold">{supply.title}</div>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="border-2 border-orange-500">
-                    {supply.category}
-                  </td>
-                  <td className="border-2 border-orange-500">
-                    ${supply.amount}
-                  </td>
-                  <td className="border-2 border-orange-500">
-                    <div className="flex flex-col justify-end">
-                      <button onClick={()=>deleteSupply(supply._id)} className=" btn  flex items-center gap-2 text-[15px] text-red-500 font-semibold border-[1px] bg-slate-400 hover:bg-slate-500 hover:text-red-700 duration-500 rounded-md p-1">
-                        Delete
-                        <MdAutoDelete />
-                      </button>
-                      <button
-                        onClick={() => setModal((prev) => !prev)}
-                        className={` btn  flex items-center gap-2 text-[15px] text-green-700 font-semibold border-[1px] bg-slate-400 hover:bg-slate-500 hover:text-green-900 duration-500 rounded-md p-1`}
-                      >
-                        Update
-                        <FaEdit />
-                      </button>
-                      {modal && (
-                        <Modal modal={modal} setModal={setModal}>
-                          <SupplyForm></SupplyForm>
-                        </Modal>
-                      )}
-                      <Link
-                        to={`/dashboard/supply-details/${supply._id}`}
-                        className=" btn  flex items-center gap-2 text-[15px] text-blue-500 font-semibold border-[1px] bg-slate-400 hover:bg-slate-500 hover:text-blue-700 duration-500 rounded-md p-1"
-                      >
-                        Details
-                        <FcViewDetails />
-                      </Link>
-                      {/* <Link
+                    </td>
+                    <td className="border-2 border-orange-500">
+                      {supply.category}
+                    </td>
+                    <td className="border-2 border-orange-500">
+                      ${supply.amount}
+                    </td>
+                    <td className="border-2 border-orange-500">
+                      <div className="flex flex-col justify-end">
+                        <button
+                          onClick={() => handleDelete(supply._id)}
+                          className=" btn  flex items-center gap-2 text-[15px] text-red-500 font-semibold border-[1px] bg-slate-400 hover:bg-slate-500 hover:text-red-700 duration-500 rounded-md p-1"
+                        >
+                          Delete
+                          <MdAutoDelete />
+                        </button>
+                        <button
+                          onClick={() => setModal((prev) => !prev)}
+                          className={` btn  flex items-center gap-2 text-[15px] text-green-700 font-semibold border-[1px] bg-slate-400 hover:bg-slate-500 hover:text-green-900 duration-500 rounded-md p-1`}
+                        >
+                          Update
+                          <FaEdit />
+                        </button>
+
+                        <Link
+                          to={`/dashboard/supply-details/${supply._id}`}
+                          className=" btn  flex items-center gap-2 text-[15px] text-blue-500 font-semibold border-[1px] bg-slate-400 hover:bg-slate-500 hover:text-blue-700 duration-500 rounded-md p-1"
+                        >
+                          Details
+                          <FcViewDetails />
+                        </Link>
+                        {/* <Link
                         to="/dashboard/create-supply"
                         className=" btn  flex items-center gap-2 text-[15px] text-purple-500 font-semibold border-[1px] bg-slate-400 hover:bg-slate-500 hover:text-purple-700 duration-500 rounded-md p-1"
                       >
                         Add Supply
                         <MdAddCircle />
                       </Link> */}
-                    </div>
-                  </td>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th className=" text-xl font-bold text-orange-500">
+                    Total Supply:${totalAmount}+
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-            <tfoot>
-              <tr>
-                <th className=" text-xl font-bold text-orange-500">
-                  Total Supply:${totalAmount}+
-                </th>
-              </tr>
-            </tfoot>
-          </table>
+              </tfoot>
+            </table>
+          </div>
         </div>
       </div>
+      {modal && (
+        <Modal modal={modal} setModal={setModal}>
+          <SupplyForm></SupplyForm>
+        </Modal>
+      )}
     </div>
   );
 };
